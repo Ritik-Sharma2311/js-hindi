081c89dae214fbba276d0f5918f59b22d71309f7
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-01-02 18:57:59 +0000

    Conversion & Operaton of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,12 +2,12 @@ let score = "Ritik"
 
 // const {score} = req.body
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN (Not a Number)
@@ -18,7 +18,7 @@ let isLoggedIn = "ritik"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,5 +27,48 @@ console.log(booleanIsLoggedIn);
 let someNumber =33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********************************OPERATIONS*****************************************/
+
+let value =3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power(2 raise to the power 3)
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Ritik"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");  //messy code not appreciated
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true);
+
+// console.log(+true);
+// console.log(+"");   //messy code not appreciated
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);   // preffix/postfix Value.
+
+
+
