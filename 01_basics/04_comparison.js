42d3a6b4d7ee2f157e4d44a3571278cc1ab5f110
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-01-02 19:21:52 +0000

    Comparison in Datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // comparison(<,>,<=) & equality(==) works differently in JavaScript. 
+
+console.log(undefined == 0);
+// we avoid such type of comparison in different types of dataypes to avoid confusion. Again Value of Clean Code is Our priority!!!
+
+// === strict check
+console.log("2" === 2);
