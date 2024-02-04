0cee5e56c2625fbd037e0e0a1759a5f417880a0b
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-02-04 16:58:08 +0000

    Classes & Oops

---
+++ 10_classes_and_Oops/notes.md
@@ -0,0 +1,22 @@
+# javascript and classes
+ 
+## OOP
+-collection of properties & methods
+-toLowerCase, toUpperCase, Promise
+
+## why use OOP
+
+
+## parts of OOP
+-Object literal
+
+-Constructor Function
+-Prototypes
+-Classes
+-Instances (new, this)
+
+## 4 pillars 
+Abstraction
+Encapsulation
+ Inheritance
+Polymorphism
---
+++ 10_classes_and_Oops/oop.js
@@ -0,0 +1,42 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+     //console.log("Got user Details from Database");
+     //console.log(`Username: ${this.username}`);
+     console.log(this);
+    }
+}
+console.log(user.username);
+// console.log(user.getUserDetails());
+console.log(this);
+
+
+// Constructor Function
+
+// const promiseOne = new Promise()
+// const date = new Date()
+// here new is the Constructor Function
+
+
+function User(username, loginCount, isLoggedIn){
+ this.username = username;
+ this.loginCount = loginCount;
+ this.isLoggedIn = isLoggedIn;
+
+ this.greetings = function(){
+    console.log(`Welcome ${this.username}`);
+ }
+
+ return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new  User("chaiaurCode" , 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
+
+
+// new -- empty instance is created => 1.NEW OBJECT Created, 2.Constructor Function is called , 3. this() is injected/passed on in new
